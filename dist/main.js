ccbb6c37ebc6d0ecd401b844653ed1960a522956
Timon <Timofey@mail.com>
2021-10-31 22:33:04 +0400

    initial webpack setup

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log({leatherStripe:2,ironIgnot:1,refineMoonstone:4});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const elevenShieldRecipe = {
+  leatherStripe: 2,
+  ironIgnot: 1,
+  refineMoonstone: 4,
+};
+
+console.log(elevenShieldRecipe)
